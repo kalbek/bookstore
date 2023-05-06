3ba82d2c87a324c473c5d62f1e1c310c89859873
kalbek <access2kaleb@gmail.com>
2023-05-06 05:55:45 +0300

    Create form to add new books.

--- src/components/pages/Books.jsx
+++ src/components/pages/Books.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 const Books = () => {
   const bookList = [
-    { title: 'Count of monte cresto', author: 'jean marques' },
-    { id: 2, title: 'Romeo and Juliet', author: 'Willian Shekspear' },
+    { title: 'The Count of monte cresto', author: 'Alexandre Dumas' },
+    { id: 2, title: 'Romeo and Juliet', author: 'William Shakespeare' },
   ];
   return (
     <>
       {bookList.map((book) => (
         <>
           <div className="books flex-column">
-            <div className="title">{book.title}</div>
-            <div className="author">{book.author}</div>
-            <div className="control flex gap-1">
+            <div className="title">
+              <h3>{book.title}</h3>
+            </div>
+            <div className="author">
+              <h4>{book.author}</h4>
+            </div>
+            <div className="control fade flex gap-1">
               <div className="edit">Comments</div>
               |
               <div className="edit">Remove</div>
@@ -22,6 +26,18 @@ const Books = () => {
           </div>
         </>
       ))}
+      <div className="add-books flex-column">
+        <div className="title fade">
+          <h2>ADD NEW BOOK</h2>
+        </div>
+        <form action="" className="flex gap-1">
+          <input type="text" placeholder="Book title" />
+          <input type="select" placeholder="Category" />
+          <button type="submit">
+            <p>ADD BOOK</p>
+          </button>
+        </form>
+      </div>
     </>
   );
 };
